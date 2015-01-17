61e933e49d5243bfe44e1a0534c677a14d54dffd
Nolan Lawson <nolan.lawson@gmail.com>
2015-01-17 18:40:56 -0500

    basic implementation

--- index.js
+++ index.js
@@ -1,17 +1,91 @@
 'use strict';
 
-var utils = require('./pouch-utils');
+var PouchPromise;
+/* istanbul ignore next */
+if (typeof window !== 'undefined' && window.PouchDB) {
+  PouchPromise = window.PouchDB.utils.Promise;
+} else {
+  PouchPromise = typeof global.Promise === 'function' ? global.Promise : require('lie');
+}
 
-exports.sayHello = utils.toPromise(function (callback) {
-  //
-  // You can use the following code to 
-  // get the pouch or PouchDB objects
-  //
-  // var pouch = this;
-  // var PouchDB = pouch.constructor;
+// this is essentially the "update sugar" function from daleharvey/pouchdb#1388
+// the diffFun tells us what delta to apply to the doc.  it either returns
+// the doc, or false if it doesn't need to do an update after all
+function upsertInner(db, docId, diffFun) {
+  return new PouchPromise(function (fulfill, reject) {
+    if (docId && typeof docId === 'object') {
+      docId = docId._id;
+    }
+    if (typeof docId !== 'string') {
+      return reject(new Error('doc id is required'));
+    }
 
-  callback(null, 'hello');
-});
+    db.get(docId, function (err, doc) {
+      if (err) {
+        /* istanbul ignore next */
+        if (err.status !== 404) {
+          return reject(err);
+        }
+        doc = {};
+      }
+      var newDoc = diffFun(doc);
+      if (!newDoc) {
+        return fulfill(doc);
+      }
+      newDoc._id = docId;
+      newDoc._rev = doc._rev;
+      fulfill(tryAndPut(db, newDoc, diffFun));
+    });
+  });
+}
+
+function tryAndPut(db, doc, diffFun) {
+  return db.put(doc).catch(function (err) {
+    /* istanbul ignore next */
+    if (err.status !== 409) {
+      throw err;
+    }
+    return upsertInner(db, doc, diffFun);
+  });
+}
+
+exports.upsert = function upsert(docId, diffFun, cb) {
+  var db = this;
+  var promise = upsertInner(db, docId, diffFun);
+  if (typeof cb !== 'function') {
+    return promise;
+  }
+  promise.then(function (resp) {
+    cb(null, resp);
+  }, cb);
+};
+
+exports.putIfNotExists = function putIfNotExists(docId, doc, cb) {
+  var db = this;
+
+  if (typeof docId !== 'string') {
+    cb = doc;
+    doc = docId;
+    docId = doc._id;
+  }
+  doc._id = docId;
+
+  var diffFun = function (existingDoc) {
+    if (existingDoc._rev) {
+      return false; // do nothing
+    }
+    return doc;
+  };
+
+  var promise = upsertInner(db, doc._id, diffFun);
+  if (typeof cb !== 'function') {
+    return promise;
+  }
+  promise.then(function (resp) {
+    cb(null, resp);
+  }, cb);
+};
+
 
 /* istanbul ignore next */
 if (typeof window !== 'undefined' && window.PouchDB) {
--- test/test.js
+++ test/test.js
@@ -3,20 +3,14 @@
 
 var Pouch = require('pouchdb');
 
-//
-// your plugin goes here
-//
-var helloPlugin = require('../');
-Pouch.plugin(helloPlugin);
+var thePlugin = require('../');
+Pouch.plugin(thePlugin);
 
 var chai = require('chai');
 chai.use(require("chai-as-promised"));
 
-//
-// more variables you might want
-//
-chai.should(); // var should = chai.should();
-require('bluebird'); // var Promise = require('bluebird');
+var should = chai.should();
+var Promise = require('bluebird');
 
 var dbs;
 if (process.browser) {
@@ -31,22 +25,307 @@ dbs.split(',').forEach(function (db) {
   tests(db, dbType);
 });
 
+function setTimeoutPromise(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
+
 function tests(dbName, dbType) {
 
   var db;
 
   beforeEach(function () {
+    this.timeout(30000);
     db = new Pouch(dbName);
     return db;
   });
   afterEach(function () {
+    this.timeout(30000);
     return Pouch.destroy(dbName);
   });
-  describe(dbType + ': hello test suite', function () {
-    it('should say hello', function () {
-      return db.sayHello().then(function (response) {
-        response.should.equal('hello');
+  describe(dbType + ': basic test suite', function () {
+    this.timeout(30000);
+
+    it('should upsert a new doc', function () {
+      return db.upsert('myid', function () {
+        return {some: 'doc'};
+      }).then(function () {
+        return db.get('myid');
+      }).then(function (doc) {
+        should.exist(doc._rev);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'myid',
+          some: 'doc'
+        });
       });
     });
+
+    it('should upsert a new doc, with callbacks', function (done) {
+      return db.upsert('myid', function () {
+        return {some: 'doc'};
+      }, function (err) {
+        if (err) {
+          return done(err);
+        }
+        db.get('myid', function (err, doc) {
+          if (err) {
+            return done(err);
+          }
+          should.exist(doc._rev);
+          delete doc._rev;
+          doc.should.deep.equal({
+            _id: 'myid',
+            some: 'doc'
+          });
+          done();
+        });
+      });
+    });
+
+    it('should upsert a new doc in parallel', function () {
+
+      function diff() {
+        return {some: 'doc'};
+      }
+
+      var promises = [
+        db.upsert('myid', diff),
+        db.upsert('myid', diff),
+        db.upsert('myid', diff)
+      ];
+
+      return Promise.all(promises).then(function () {
+        return db.get('myid');
+      }).then(function (doc) {
+        should.exist(doc._rev);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'myid',
+          some: 'doc'
+        });
+      });
+    });
+
+    it('should upsert a new doc in parallel, still gen-1', function () {
+
+      function diff(doc) {
+        if (doc.version) {
+          return false;
+        }
+        return {version: 1};
+      }
+
+      var promises = [
+        db.upsert('myid', diff),
+        db.upsert('myid', diff),
+        db.upsert('myid', diff)
+      ];
+
+      return Promise.all(promises).then(function () {
+        return db.get('myid');
+      }).then(function (doc) {
+        doc._rev.should.match(/1-/); // still gen-1
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'myid',
+          version: 1
+        });
+      });
+    });
+
+    it('should upsert a new doc, using an object', function () {
+      return db.upsert({_id: 'myid'}, function () {
+        return {some: 'doc'};
+      }).then(function () {
+        return db.get('myid');
+      }).then(function (doc) {
+        should.exist(doc._rev);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'myid',
+          some: 'doc'
+        });
+      });
+    });
+
+    it('should throw if no doc _id', function () {
+      return db.upsert({}, function () {
+        return {some: 'doc'};
+      }).then(function () {
+        throw new Error('should not be here');
+      }, function (err) {
+        should.exist(err);
+      });
+    });
+
+    it('should upsert an existing doc', function () {
+      return db.upsert('myid', function () {
+        return {some: 'doc'};
+      }).then(function () {
+        return db.upsert('myid', function (doc) {
+          doc.version = 2;
+          return doc;
+        });
+      }).then(function () {
+        return db.get('myid');
+      }).then(function (doc) {
+        should.exist(doc._rev);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'myid',
+          some: 'doc',
+          version: 2
+        });
+      });
+    });
+
+    it('should not upsert if diffFun returns falsy', function () {
+      return db.upsert('myid', function () {
+        return {some: 'doc'};
+      }).then(function () {
+        return db.upsert('myid', function () {
+          return false;
+        });
+      }).then(function () {
+        return db.get('myid');
+      }).then(function (doc) {
+        should.exist(doc._rev);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'myid',
+          some: 'doc'
+        });
+      });
+    });
+
+    it('should create a new doc, with sugar', function () {
+      return db.putIfNotExists({_id: 'foo', hey: 'yo'}).then(function () {
+        return db.get('foo');
+      }).then(function (doc) {
+        should.exist(doc._rev);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'foo',
+          hey: 'yo'
+        });
+      });
+    });
+
+    it('should create a new doc, with sugar and callbacks', function (done) {
+      db.putIfNotExists({_id: 'foo', hey: 'yo'}, function (err) {
+        if (err) {
+          return done(err);
+        }
+        db.get('foo', function (err, doc) {
+          if (err) {
+            return done(err);
+          }
+          should.exist(doc._rev);
+          delete doc._rev;
+          doc.should.deep.equal({
+            _id: 'foo',
+            hey: 'yo'
+          });
+          done();
+        });
+      });
+    });
+
+    it('should create a new doc, with sugar and callbacks 2', function (done) {
+      db.putIfNotExists('foo', {hey: 'yo'}, function (err) {
+        if (err) {
+          return done(err);
+        }
+        db.get('foo', function (err, doc) {
+          if (err) {
+            return done(err);
+          }
+          should.exist(doc._rev);
+          delete doc._rev;
+          doc.should.deep.equal({
+            _id: 'foo',
+            hey: 'yo'
+          });
+          done();
+        });
+      });
+    });
+
+    it('should not recreate a doc, with sugar', function () {
+      return db.putIfNotExists({_id: 'foo', hey: 'yo'}).then(function () {
+        return db.putIfNotExists({_id: 'foo', another: 'thing'});
+      }).then(function () {
+        return db.get('foo');
+      }).then(function (doc) {
+        doc._rev.should.match(/1-/);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'foo',
+          hey: 'yo'
+        });
+      });
+    });
+
+    it('should not recreate a doc, with sugar 2', function () {
+      return db.putIfNotExists('foo', {hey: 'yo'}).then(function () {
+        return db.putIfNotExists('foo', {another: 'thing'});
+      }).then(function () {
+        return db.get('foo');
+      }).then(function (doc) {
+        doc._rev.should.match(/1-/);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'foo',
+          hey: 'yo'
+        });
+      });
+    });
+
+    it('should not recreate a doc, with sugar, in parallel', function () {
+
+      var promises = [
+        db.putIfNotExists('foo', {hey: 'yo'}),
+        db.putIfNotExists('foo', {hey: 'yo'}),
+        db.putIfNotExists('foo', {hey: 'yo'})
+      ];
+
+      return Promise.all(promises).then(function () {
+        return db.get('foo');
+      }).then(function (doc) {
+        doc._rev.should.match(/1-/);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'foo',
+          hey: 'yo'
+        });
+      });
+    });
+
+    it('should not recreate a doc, with sugar, first wins', function () {
+
+      var promises = [
+        db.putIfNotExists('foo', {hey: 'yo'}),
+        setTimeoutPromise(500).then(function () {
+          return db.putIfNotExists('foo', {hey: 'dude'});
+        }),
+        setTimeoutPromise(1000).then(function () {
+          return db.putIfNotExists('foo', {hey: 'sista'});
+        })
+      ];
+
+      return Promise.all(promises).then(function () {
+        return db.get('foo');
+      }).then(function (doc) {
+        doc._rev.should.match(/1-/);
+        delete doc._rev;
+        doc.should.deep.equal({
+          _id: 'foo',
+          hey: 'yo'
+        });
+      });
+    });
+
   });
 }
