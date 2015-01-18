64da88e933fcaeb7be90a7993db75d470d8c7c1e
Nolan Lawson <nolan.lawson@gmail.com>
2015-01-17 20:34:29 -0500

    1.0.2

--- dist/pouchdb.upsert.js
+++ dist/pouchdb.upsert.js
@@ -14,9 +14,6 @@ if (typeof window !== 'undefined' && window.PouchDB) {
 // the doc, or false if it doesn't need to do an update after all
 function upsertInner(db, docId, diffFun) {
   return new PouchPromise(function (fulfill, reject) {
-    if (docId && typeof docId === 'object') {
-      docId = docId._id;
-    }
     if (typeof docId !== 'string') {
       return reject(new Error('doc id is required'));
     }
@@ -46,7 +43,7 @@ function tryAndPut(db, doc, diffFun) {
     if (err.status !== 409) {
       throw err;
     }
-    return upsertInner(db, doc, diffFun);
+    return upsertInner(db, doc._id, diffFun);
   });
 }
 
@@ -69,7 +66,6 @@ exports.putIfNotExists = function putIfNotExists(docId, doc, cb) {
     doc = docId;
     docId = doc._id;
   }
-  doc._id = docId;
 
   var diffFun = function (existingDoc) {
     if (existingDoc._rev) {
@@ -78,7 +74,7 @@ exports.putIfNotExists = function putIfNotExists(docId, doc, cb) {
     return doc;
   };
 
-  var promise = upsertInner(db, doc._id, diffFun);
+  var promise = upsertInner(db, docId, diffFun);
   if (typeof cb !== 'function') {
     return promise;
   }
